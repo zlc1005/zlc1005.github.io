d73ef549b327d19c5bc49c8625eb06b31cde4b6f
zlc1005 <92490758+zlc1005@users.noreply.github.com>
2021-10-22 12:54:00 -0700

    Update sketch.js

--- garbage/sketch.js
+++ garbage/sketch.js
@@ -1,31 +1,3 @@
-var direction=''
-
-function preload() {
-    var backgroundImg = loadImage("background.jpg")
-    var apple = loadImage("apple.jpg")
-    var recy = loadImage('a.bmp')
-    var non = loadImage('b.bmp')
-    var kit = loadImage('c.bmp')
-    var har = loadImage('d.bmp')
-    var other =loadImage('e.bmp')
-    var banana = loadImage('xj.jpg')
-    var coke = loadImage('kl.jpg')
-    var dhj = loadImage('dhj.jpg')
-    var apple = loadImage('apple.jpg')
-    var news = loadImage('bz.jpg')
-    var battery = loadImage('dc.jpg')
-    var pizza = loadImage('pizza.png')
-    var pc = loadImage('dn.jpg')
-    var paper = loadImage('cz.webp')
-    var battle = loadImage('slsp.jpg')
-    var light = loadImage('dp.jpg')
-}
-
-function setup() {}
-
-function draw() {
-    image(backgroundImg, 0, 0)//,width, height)
-}
 direction=''
 
 function preload() {
