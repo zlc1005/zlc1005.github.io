6901af6a1cb1b5123f8a982cb9787af2b5cc47f5
zlc1005 <92490758+zlc1005@users.noreply.github.com>
2021-10-27 17:28:42 -0700

    Update sketch.js

--- garbage/sketch.js
+++ garbage/sketch.js
@@ -1,4 +1,3 @@
-
 function preload() { // preload() is load all the images
     backgroundImg = loadImage("https://zlc1005.github.io/garbage/background.jpg")
     recy = createImg('https://zlc1005.github.io/garbage/a.bmp', 'alt')
@@ -29,8 +28,11 @@ function preload() { // preload() is load all the images
 }
 imgxy = 100, 100// set the position of the image
 d = 0//点击次数
-w=0// wrong
+w = 0// wrong
 r = 0//right次数
+$(function () {
+    $('#spinner').fadeOut();
+})
 function setup() { // setup() is set up the canvas
     createCanvas(1000, 700) // set the canvas size
     background(backgroundImg)// set the background
