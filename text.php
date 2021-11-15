de1dd50ce6d4227c059df9d63fcf1ebea73b48b5
zlc1005 <92490758+zlc1005@users.noreply.github.com>
2021-11-15 09:42:13 -0800

    Create text.php

---
+++ text.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php
+    $json_strings=json_encode(array("a"=>'aa','b'=>'aaa'))
+    file_put_contents("text.json",$json_strings);
+    echo 'Success'
+    ?>
+
+</body>
+</html>  
