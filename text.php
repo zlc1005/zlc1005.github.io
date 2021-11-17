03ce84f1fbf7b12cdd939d2808d7cff3c80ae3e5
zlc1005 <92490758+zlc1005@users.noreply.github.com>
2021-11-16 17:04:51 -0800

    Update text.php

--- text.php
+++ text.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-    <?php
-    $json_strings=json_encode(array("a"=>'aa','b'=>'aaa'))
-    file_put_contents("text.json",$json_strings);
-    echo 'Success'
-    ?>
-
+<?php
+$json_strings=json_encode(array("a"=>'aa','b'=>'aaa'))
+file_put_contents("text.json",$json_strings);
+echo 'Success'
+?>
 </body>
 </html>  
