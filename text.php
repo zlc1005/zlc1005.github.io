a4c3d957756224afd0713671d774b98c739bf915
zlc1005 <92490758+zlc1005@users.noreply.github.com>
2021-11-16 17:30:50 -0800

    Update text.php

--- text.php
+++ text.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 <body>
+
+<h1>My first PHP page</h1>
+
+
 <?php
-$json_strings=json_encode(array("a"=>'aa','b'=>'aaa'))
-file_put_contents("text.json",$json_strings);
-echo 'Success'
+echo "<h1> wow</h1>";
+$x = 5;
+$y = 4;
+$age = json_encode(array("aaaaa"=>"1aaa235", "aaaf"=>"4aaaa4", "caaac"=>"aaa6"));
+echo $age;
+file_put_contents("text.json",$age,FILE_APPEND);
+//json_encode(
 ?>
+
 </body>
-</html>  
+</html>
